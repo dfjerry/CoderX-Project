369eecae56f7362757b07b9ae5a62b24f405caf4
dfjerry <53176182+dfjerry@users.noreply.github.com>
2020-03-19 11:58:29 +0700

    sayHi method for dog

--- Dog.js
+++ Dog.js
@@ -1,3 +1,4 @@
+var chalk = require('chalk');
 function Dog(name){
 	this.name = name;
 	this.stomatch = [];
@@ -5,3 +6,7 @@ function Dog(name){
 Dog.prototype.eat = function(cat){
 	this.stomatch.push(cat);
 }
+Dog.prototype.sayHi = function(){
+	console.log('Hi i\'m a dog' + chalk.blue(this.name));
+}
+module.exports = Dog;
